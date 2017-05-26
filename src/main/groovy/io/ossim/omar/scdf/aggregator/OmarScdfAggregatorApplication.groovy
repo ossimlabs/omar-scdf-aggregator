bf1b4afe80753445a95f1e076f6a930fa1ff1677
melbsurfer <melbsurfer@gmail.com>
2017-05-26 13:47:18 -0400

    Refactored as filter

--- src/main/groovy/io/ossim/omar/scdf/aggregator/OmarScdfAggregatorApplication.groovy
+++ src/main/groovy/io/ossim/omar/scdf/aggregator/OmarScdfAggregatorApplication.groovy
@@ -1,15 +1,19 @@
 package io.ossim.omar.scdf.aggregator
 
+import groovy.json.JsonOutput
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.stream.annotation.EnableBinding
 import org.springframework.cloud.stream.annotation.StreamListener
 import org.springframework.cloud.stream.messaging.Processor
 import org.springframework.core.io.support.ResourcePatternResolver
+import org.springframework.integration.annotation.Filter
+import org.springframework.messaging.Message
 import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.io.ResourceLoader
 import org.springframework.core.io.Resource
+import groovy.json.JsonSlurper
 
 
 @SpringBootApplication
@@ -26,59 +30,56 @@ class OmarScdfAggregatorApplication {
 	@Autowired
 	private ResourcePatternResolver resourcePatternResolver
 
-	@StreamListener(Processor.INPUT) @SendTo(Processor.OUTPUT)
-	String transform(ReceivedAwsData receivedAwsData){
+	@Filter(inputChannel = Processor.INPUT, outputChannel = Processor.OUTPUT)
+	public boolean filter(Message<?> message) {
+
+		def filesToDownload
+		def payload = new JsonSlurper().parseText(message.payload)
+		def bucketName = payload.bucket
+
+		def fileName = payload.filename[0..payload.filename.lastIndexOf('.') - 1]
+		def fileExtension = payload.filename[payload.filename.lastIndexOf('.')..payload.filename.length() - 1]
+		def fileToLookFor = ""
+
+		// TODO: Filter file extensions via config
+		if (".zip".equals(fileExtension)) {
+			fileToLookFor = "${fileName}.txt"
+		} else if (".txt".equals(fileExtension)) {
+			fileToLookFor = "${fileName}.zip"
+		}
+		// else wtf kind of file?
+
+		def s3 = "s3://${bucketName}/${fileToLookFor}"
 
 		try {
 
-			Resource[] allZipFilesInFolder = this.resourcePatternResolver.getResources("s3://omar-dropbox/*.zip")
-			String outputMessage
+			Resource fileFoundInS3 = this.resourcePatternResolver.getResource(s3)
+			println(fileFoundInS3)
 
-//			allZipFilesInFolder.each {
-//
-//				println "Incoming SQS filename: ${receivedAwsData.filename}"
-//				println 'Checking against Bucket filename: ' + it.filename
-//
-//				def f = it.filename
-//				println f
-//
-//				if(f == receivedAwsData.filename) {
-//
-//					println "##################"
-//					println "Bingo!!! ${f} is here in the bucket!!!! "
-//					println "##################"
-//
-//					outputMessage = receivedAwsData.filename + " " + receivedAwsData.bucket
-//				}
-//				else {
-//
-//					outputMessage = "File not found yet!"
-//
-//				}
-//
-//			}
+			// The other file exists! Put both files in a JSON array to send to next processor
+			def zipFile = new BucketFile()
+			zipFile.filename = "${fileName}.zip"
+			zipFile.bucket = bucketName
+			def txtFile = new BucketFile()
+			txtFile.filename = "${fileName}.txt"
+			txtFile.bucket = bucketName
 
-			allZipFilesInFolder.find { element ->
-
-				println element.filename
-				outputMessage = element.filename
-				return (element.filename == receivedAwsData.filename)
-
-			}
-
-			println "Found ${outputMessage}"
-			return outputMessage + " " + receivedAwsData.bucket
-
+			filesToDownload = JsonOutput.toJson([zipFile, txtFile])
+			println(filesToDownload)
 		}
-		catch(Exception ex){
+		catch (Exception ex) {
 
-			println "Exception ${ex}"
+			// No file found in S3, die
+			println ex.printStackTrace()
 
 		}
 
-		//return receivedAwsData.filename + " " + receivedAwsData.bucket
-
+		return filesToDownload != null
 
 	}
 
+	private final class BucketFile {
+		String filename;
+		String bucket;
+	}
 }
